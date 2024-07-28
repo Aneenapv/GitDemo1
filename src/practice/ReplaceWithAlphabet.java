71e9508d3c067b1f7171454a6622ff3fe5095b08
Aneena p.v <pvaneena@gmail.com>
2024-07-27 20:16:20 -0400

    Programs

---
+++ src/practice/ReplaceWithAlphabet.java
@@ -0,0 +1,31 @@
+package practice;
+
+	import java.util.Scanner;
+	
+	public class ReplaceWithAlphabet {
+		
+		//Write a program to replace the numbers in an array with alphabets.
+
+	    public static void main(String[] args)
+	    {
+	        Scanner scanner = new Scanner(System.in);
+	        System.out.println("Enter the size of the array: ");
+	        int size = scanner.nextInt();
+	        
+	        int[] array = new int[size];
+	        System.out.println("Enter the elements of the array (0-25): ");
+	        for (int i = 0; i < size; i++) {
+	            array[i] = scanner.nextInt();
+	        }
+	        
+	        System.out.println("The array with numbers replaced by alphabets is: ");
+	        for (int num : array) {
+	            char alphabet = (char) ('A' + num);
+	            System.out.print(alphabet + " ");
+	        }
+	        scanner.close();
+	    }
+	}
+
+
+
